40253597dd3274c992a96197aefe9fd5c0682100
Alex <cigan@ua.fm>
2021-04-18 20:36:07 +0300

    create new project, add two new components

---
+++ angular-first-app/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { PostComponent } from './post/post.component';
+
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  posts: Post[] = [
+    {
+      title: 'Хочу выучить Angular компоненты',
+      text: 'Я все еще учу компоненты',
+      id: 1,
+    },
+    {
+      title: 'Следующий блок',
+      text: 'Следующий блок будет про дерективы и пайпы',
+      id: 2,
+    },
+    {
+      title: 'Следующий блок',
+      text: 'Следующий блок будет про дерективы и пайпы',
+      id: 3,
+    },
+  ];
+}
