b201e271967f3fe6b6504ac64864215a664ee29c
Alex <cigan@ua.fm>
2021-04-20 17:32:22 +0300

    use @HostListener to change properties dynamically (click, mouseenter...)

--- angular-first-app/src/app/app.module.ts
+++ angular-first-app/src/app/app.module.ts
@@ -5,10 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PostFormComponent } from './post-form/post-form.component';
 import { PostComponent } from './post/post.component';
-import {StyleDirectives} from './directives/style.directives';
+import {StyleDirective} from './directives/style.directives';
 
 @NgModule({
-    declarations: [AppComponent, PostFormComponent, PostComponent, StyleDirectives],
+  declarations: [AppComponent, PostFormComponent, PostComponent, StyleDirective],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
--- angular-first-app/src/app/directives/style.directives.ts
+++ angular-first-app/src/app/directives/style.directives.ts
@@ -1,13 +1,28 @@
-import {Directive, ElementRef, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
+import {colors} from '@angular/cli/utilities/color';
 
 
 @Directive({
   selector: '[appStyle]'
 })
 
-export class StyleDirectives {
+export class StyleDirective {
   constructor(private el: ElementRef, private rend: Renderer2) {
     // this.el.nativeElement.style.color = 'red';
+    console.log('render2: ', rend);
     this.rend.setStyle(this.el.nativeElement, 'color', 'blue');
+  }
+
+  @HostListener('click', ['$event.target']) onClick(event: Event): void {
+    // console.log(event);
+    this.rend.setStyle(this.el.nativeElement, 'color', 'yellow');
+  }
+
+  @HostListener('mouseenter') onMouseEnter(): void {
+    this.rend.setStyle(this.el.nativeElement, 'color', 'green');
+  }
+
+  @HostListener('mouseleave') onMouseLeave(): void {
+    this.rend.setStyle(this.el.nativeElement, 'color', null);
   }
 }
