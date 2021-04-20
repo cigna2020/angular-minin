64f10cbe767dc69877fbd841ea8158ad4234c1d6
Alex <cigan@ua.fm>
2021-04-20 11:33:20 +0300

    add Lifecycle hooks methods and log every hook

--- angular-first-app/src/app/post/post.component.ts
+++ angular-first-app/src/app/post/post.component.ts
@@ -1,24 +1,70 @@
 import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewInit,
   Component,
   ContentChild,
+  DoCheck,
   ElementRef,
   Input,
+  OnChanges,
   OnInit,
+  SimpleChanges,
+  AfterViewChecked,
+  OnDestroy,
+  Output,
+  EventEmitter
 } from '@angular/core';
 import { Post } from '../app.component';
 
+// tslint:disable-next-line:no-conflicting-lifecycle
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss'],
 })
-export class PostComponent implements OnInit {
+export class PostComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy {
   @Input() myPost: Post;
-  @ContentChild('info', { static: true }) infoRef: ElementRef;
+  // tslint:disable-next-line:no-output-on-prefix
+  @ContentChild('info', { static: true })
+  infoRef: ElementRef;
 
-  constructor() {}
+  constructor() {
+    console.log('constructor');
+  }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges', changes);
+  }
   ngOnInit(): void {
-    console.log(this.infoRef.nativeElement);
+    // console.log(this.infoRef.nativeElement);
+    console.log('ngOnInit');
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
   }
 }
