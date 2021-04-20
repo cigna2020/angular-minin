63d32c99bb75a5422d509a0ea499ef63114d2f77
Alex <cigan@ua.fm>
2021-04-20 17:58:28 +0300

    pass data into Directive using @Input

--- angular-first-app/src/app/directives/style.directives.ts
+++ angular-first-app/src/app/directives/style.directives.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
 import {colors} from '@angular/cli/utilities/color';
 
 
@@ -7,6 +7,9 @@ import {colors} from '@angular/cli/utilities/color';
 })
 
 export class StyleDirective {
+  @Input('appStyle') newColor: string;
+  @Input() dStyles: {border?: string, fontWeight?: string, borderRadius?: string};
+
   constructor(private el: ElementRef, private rend: Renderer2) {
     // this.el.nativeElement.style.color = 'red';
     console.log('render2: ', rend);
@@ -16,13 +19,21 @@ export class StyleDirective {
   @HostListener('click', ['$event.target']) onClick(event: Event): void {
     // console.log(event);
     this.rend.setStyle(this.el.nativeElement, 'color', 'yellow');
+    this.rend.setStyle(this.el.nativeElement, 'border', this.dStyles.border);
+    this.rend.setStyle(this.el.nativeElement, 'fontWeight', this.dStyles.fontWeight);
+    this.rend.setStyle(this.el.nativeElement, 'borderRadius', this.dStyles.borderRadius);
+
   }
 
   @HostListener('mouseenter') onMouseEnter(): void {
-    this.rend.setStyle(this.el.nativeElement, 'color', 'green');
+    this.rend.setStyle(this.el.nativeElement, 'color', this.newColor);
   }
 
   @HostListener('mouseleave') onMouseLeave(): void {
     this.rend.setStyle(this.el.nativeElement, 'color', null);
+    this.rend.setStyle(this.el.nativeElement, 'border', null);
+    this.rend.setStyle(this.el.nativeElement, 'fontWeight', null);
+    this.rend.setStyle(this.el.nativeElement, 'borderRadius', null);
+
   }
 }
