8780049e5aaa3ed77102767faa84e8341be0c0ef
Alex <cigan@ua.fm>
2021-04-29 17:27:12 +0300

    PATH routing add and using <router-outlet>

--- angular-first-app/src/app/app-routing.module.ts
+++ angular-first-app/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+import {PostsComponent} from './posts/posts.component';
+import {FormsComponent} from './forms/forms.component';
+import {HttpclientComponent} from './httpclient/httpclient.component';
+
+// '' === http://localhost:4200
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'posts', component: PostsComponent},
+  {path: 'forms', component: FormsComponent},
+  {path: 'requests', component: HttpclientComponent}
+];
 
 @NgModule({
-  imports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],  // регистрация роутинга!!!
   exports: [RouterModule]
 })
 
